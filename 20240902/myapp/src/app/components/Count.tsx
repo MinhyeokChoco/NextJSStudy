19b8527d8032989ec4b25d692450b6ba44a44e44
MinhyeokChoco <smile_forever@nate.com>
2024-09-04 20:36:24 +0900

    modify

--- 20240902/myapp/src/app/components/Count.tsx
+++ 20240902/myapp/src/app/components/Count.tsx
@@ -8,6 +8,10 @@ const Count = () => {
     // HTML 완성할 때 한번 로직이 호출되고 서버에서 한번 찍히고 브라우저에서도 동작한다.
     console.log(1234);
 
+    // "use client" 없으면
+    // 만약에 서버 컴포넌트에서 사용하는 변수를 선언하면 브라우저에 노출될 일은 없다.
+    const a = 1;
+
     const handlerIncrement = () => {
         setCount(count + 1)
     }
